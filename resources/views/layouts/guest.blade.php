2646f8ef2ff0404ca44e19b906019bb86d330587
rioaldie <rioaldierwanto@gmail.com>
2024-09-05 19:23:20 +0700

    redesign auth login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'apotek') }}</title>
+        <title>Pharma Shop</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -18,7 +18,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                  <img src="/icons/Logo-apotek.svg" alt="">
                 </a>
             </div>
 
